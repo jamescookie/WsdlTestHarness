ced90a225a15a1a9df2a4370f710bba09b1b736f
James <James@be3fd238-c909-4725-b312-79b88f520c4f>
2007-09-03 08:00:45 +0000

    Added a functional test
    git-svn-id: http://192.168.1.66/svn/Java/trunk/wsdltestharness@137 be3fd238-c909-4725-b312-79b88f520c4f

--- src/com/aol/wsdl/FieldDescriptor.java
+++ src/com/aol/wsdl/FieldDescriptor.java
@@ -5,6 +5,7 @@ import org.apache.axis.wsdl.symbolTable.Parameter;
 
 import javax.xml.namespace.QName;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 
 public class FieldDescriptor extends ArrayList<FieldDescriptor> {
@@ -16,6 +17,22 @@ public class FieldDescriptor extends ArrayList<FieldDescriptor> {
     private String value;
     private String formName;
     private QName qname;
+    private String validationMessage = "";
+
+    @Deprecated
+    public FieldDescriptor() {
+    }
+
+    public FieldDescriptor(boolean primitive, QName qname, String name, String formName, int depth, String javaType) throws ClassNotFoundException {
+        this.primitive = primitive;
+        this.qname = qname;
+        this.name = name;
+        this.formName = formName;
+        this.depth = depth;
+        if (primitive) {
+            setJavaType(javaType);
+        }
+    }
 
     public String getName() {
         return name;
@@ -73,18 +90,35 @@ public class FieldDescriptor extends ArrayList<FieldDescriptor> {
         return qname;
     }
 
+    public boolean getValidationMessageRequired() {
+        return validationMessage != null;
+    }
+
+    public String getValidationMessage() {
+        return validationMessage;
+    }
+
+    public void setValidationMessage(String validationMessage) {
+        this.validationMessage = validationMessage;
+    }
+
     public Class getJavaType() {
         return javaType;
     }
 
     public void setJavaType(String javaTypeName) throws ClassNotFoundException {
-        javaTypeName = new String(new char[]{javaTypeName.charAt(0)}).toUpperCase() + javaTypeName.substring(1);
-        if ("Int".equals(javaTypeName)) {
-            javaTypeName = "Integer";
-        } else if ("Char".equals(javaTypeName)) {
-            javaTypeName = "Character";
+        if (isPrimitive()) {
+            javaTypeName = new String(new char[]{javaTypeName.charAt(0)}).toUpperCase() + javaTypeName.substring(1);
+            if ("Int".equals(javaTypeName)) {
+                javaTypeName = "Integer";
+            } else if ("Char".equals(javaTypeName)) {
+                javaTypeName = "Character";
+            } else if ("DateTime".equals(javaTypeName)) {
+                this.javaType = Class.forName("java.util.Date");
+                return;
+            }
+            this.javaType = Class.forName("java.lang." + javaTypeName);
         }
-        this.javaType = Class.forName("java.lang." + javaTypeName);
     }
 
     public void serialize(SerializationContext context) throws IOException {
@@ -100,5 +134,39 @@ public class FieldDescriptor extends ArrayList<FieldDescriptor> {
 
             context.endElement();
         }
+    }
+
+    public boolean isValid() {
+        boolean retValue = true;
+        if (isPrimitive()) {
+            Class javaType = getJavaType();
+            String value = getValue();
+            if (Character.class.equals(javaType)) {
+                retValue = value.length() == 1;
+            } else {
+                try {
+                    Constructor constructor = javaType.getConstructor(String.class);
+                    constructor.newInstance(value);
+                } catch (Exception e) {
+                    retValue = false;
+                }
+            }
+            if (!retValue) {
+                setValidationMessage("Unable to create " + javaType + " from '" + value + "'");
+            }
+        } else {
+            for (FieldDescriptor child : this) {
+                if (!child.isValid()) {
+                    retValue = false;
+                }
+            }
+        }
+        return retValue;
+    }
+
+    public String toString() {
+        return "FieldDescriptor: " +
+                " primitive: " + primitive +
+                super.toString();
     }
 }
---
+++ test/com/aol/wsdl/FunctionalTest.java
@@ -0,0 +1,32 @@
+package com.aol.wsdl;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class FunctionalTest {
+
+    @Test
+    public void shouldShowCorrectOperations() throws Exception {
+        ServiceLocator serviceLocator = new ServiceLocator("http://samples.gotdotnet.com/quickstart/aspplus/samples/services/MathService/VB/MathService.asmx?WSDL");
+        List<String> names = serviceLocator.getOperationNames();
+        Assert.assertEquals(4, names.size());
+        for (String name : names) {
+            FieldDescriptor descriptor = serviceLocator.createFieldDescriptor(name);
+            descriptor.get(0).setValue("6");
+            descriptor.get(1).setValue("3");
+            String s = serviceLocator.invoke(name, descriptor);
+            if (name.equalsIgnoreCase("add")) {
+                Assert.assertTrue(s.contains("9"));
+            } else if (name.equalsIgnoreCase("subtract")) {
+                Assert.assertTrue(s.contains("3"));
+            } else if (name.equalsIgnoreCase("divide")) {
+                Assert.assertTrue(s.contains("2"));
+            } else if (name.equalsIgnoreCase("multiply")) {
+                Assert.assertTrue(s.contains("18"));
+            }
+        }
+    }
+    
+}
